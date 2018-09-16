ac23e66fe3cb3d9bfbd59d6c80416de480f60144
Jhonatan Amorim <amorim.dev@gmail.com>
2018-09-16 12:00:32 -0300

    add company model

---
+++ server/models/company.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const shortId = require('shortid')
+
+const CompanySchema = new Schema({
+  _id: {
+    type: String,
+    default: shortId.generate
+  },
+  createdAt: {
+    type: Date,
+    default: new Date().getTime()
+  },
+  name: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('company', CompanySchema)
