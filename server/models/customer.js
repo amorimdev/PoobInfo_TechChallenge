c14bdbbb7c83cf3ba2ae837f8c939df7177921b9
Jhonatan Amorim <amorim.dev@gmail.com>
2018-09-16 12:09:11 -0300

    add customer model

---
+++ server/models/customer.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const autoIncrement = require('mongoose-auto-increment')
+const Schema = mongoose.Schema
+
+const CustomerSchema = new Schema({
+  companyId: {
+    type: String, ref: 'company'
+  },
+  createdAt: {
+    type: Date,
+    default: new Date().getTime()
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String
+  },
+  dob: {
+    type: Date
+  }
+})
+
+autoIncrement.initialize(mongoose.connection)
+CustomerSchema.plugin(autoIncrement.plugin, 'customer')
+
+module.exports = mongoose.model('customer', CustomerSchema)
